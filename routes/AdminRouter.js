099e39c316f2f12ff2ea604db760095ec4b650d9
Emre ŞURK <emre.surk@gmail.com>
2020-11-26 14:29:26 +0300

    HTTP Methods for RestFul

--- routes/AdminRouter.js
+++ routes/AdminRouter.js
@@ -5,9 +5,8 @@ const router = express.Router();
 const userMW = require('../middlewares/user');
 const { addLesson, adminView, deleteLesson } = require('../controllers/admin');
 
-router.get('/', userMW, adminView);
-
-router.post('/addLesson', userMW, addLesson);
-router.get('/deleteLesson/:deleteLesson', userMW, deleteLesson);
+router.get('/lesson', userMW, adminView);
+router.post('/lesson', userMW, addLesson);
+router.delete('/lesson/:lessonId', userMW, deleteLesson);
 
 module.exports = router;
